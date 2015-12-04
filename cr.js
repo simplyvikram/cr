6d5e038b061409a5cecff85af9329f123b246c4e
vikram singh <simplyvikram@gmail.com>
2015-12-04 01:26:04 -0500

    Revert "fixes to make code work"
    This reverts commit ae6baad475ca7c4ce21ff0937762de702a3e61ec.

--- cr.js
+++ cr.js
@@ -1,10 +1,9 @@
-
 /**
  * A priority queue stores a list of items but each can have a numeric priority value.
  * Items with a higher priority are dequeued before items with a lower priority.
  * Implemented as a hash of arrays where the hash keys are priority values.
  */
-function PriorityQueue() {
+function PriorityQueue(size) {
     this.store = {};	// keys are priorities, values are arrays of elements
     this.count = 0;
 
@@ -20,20 +19,9 @@ function PriorityQueue() {
 
     // returns the oldest-added value with the highest priority
     this.Pop = function() {
-        console.log("pop called");
-
-        keys = Object.keys(this.store);
-        if (keys.length > 0) {
-            maxKey = Math.max.apply(Math, keys);
-            this.count--;
-            val =  this.store[maxKey].shift();
-            if (this.store[maxKey].length == 0) {
-                delete this.store[maxKey];
-            }
-            return val;
-        } else {
-            return null;
-        }
+        maxKey = Math.max(Object.keys(this.store));
+        this.count--;
+        return this.store[maxKey].shift();
     };
 
     this.length = function() {
@@ -47,79 +35,26 @@ PriorityQueue.prototype.get_all_priorities = function() {
 
 // iterates through all the queue elements in priority-then-FIFO order
 PriorityQueue.prototype.forEach = function(callback) {
+    var keys = Object.keys(this.store).sort();
 
-    for (var key in this.store) {
-        this.store[key].forEach(function(val, indexOfVal){
-            callback(key, val, indexOfVal)
-        });
+    for (var a = keys.length; a > 0; a--) {
+        for (var b = 0; b < this.store[a].length; b++)
+            callback(this.store[a][b]);
     }
 }
 
 PriorityQueue.prototype.changePriority = function(value, newPriority) {
+    var foundItem = false;
 
-    self = this;
-    foundItem = false;
-
-    this.forEach(function(key, val, indexOfVal){
-        if (value === val) {
-            self.store[key].splice(indexOfVal, 1);
-            self.count--;
-            self.add(value, newPriority);
-            foundItem = true;
-        }
+    this.store.forEach(function(bucket) {
+        bucket.forEach(function(item, index) {
+            if (item === value) {
+                bucket.splice(index, 1);  // remove the item
+                this.add(value, newPriority);
+                foundItem = true;
+                return false;  // early exit from forEach
+            }
+        });
+        if (foundItem) return false;
     });
-    return foundItem;
-
 }
-
-PriorityQueue.prototype.show = function () {
-
-    for (var key in this.store) {
-        s = "key: " + key + ", values:";
-        this.store[key].forEach(function (val) {
-            s += " " + val
-        })
-        console.log(s);
-    }
-
-}
-
-function someFunc(key, val, indexOfVal){
-    console.log("someFunc called key: " + key + ", val:" + val + " indexOfVal:" + indexOfVal);
-
-}
-
-p = new PriorityQueue();
-
-p.add(100, 2);
-p.add(10, 2);
-p.add(1000, 2);
-
-p.add(888, 19);
-
-p.add(20, 1);
-p.add(200, 1);
-
-p.add(911, 4);
-p.add(910, 4);
-p.add(912, 4);
-console.log("Num items in queue: " + p.length());
-console.log("Current state of priority queue:");
-p.show();
-
-
-value = 944;
-priority = 8;
-console.log("Tryring to change priority for value: " + value + " to priority:" + priority);
-console.log("Priority change status: " + p.changePriority(value, priority));
-console.log("New status of priority queue");
-p.show();
-
-console.log("Now lets pop from priority queue");
-numElements = p.length();
-for (i = 0; i < numElements; i++){
-    console.log("popping " + p.Pop());
-}
-p.show();
-
-
